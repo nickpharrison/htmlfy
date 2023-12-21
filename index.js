641ce89572a225f9e0442a3f1af61c2b41d45f54
Jason Creviston <the@j4w8n.com>
2023-12-21 12:26:14 -0500

    tatu

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* Testing. */
 import { prettify } from './src/prettify'
-const html = `<form id="fury-prompt-3"     >     <!-- 
+const html = `<form id="3"     >     <!-- 
       
-This is a comment. --><!-- This is a second comment. --><label for="fury-email-0">
+This is a comment. --><!-- This is a second comment. --><label for="email-0">
 
-What's your email?</label><input id="fury-email-0" type="email" title="We need your email for verification." name="email" required><!--         This is another comment. --><label for="fury-fruits-1">What fruits do you like?</label><fieldset><input id="fury-fruits-1-0" type="checkbox" name="fruits" value="apples"><label for="fury-fruits-1-0">Apples</label><br><div><!--      This is an embedded comment. --></div><input id="fury-fruits-1-1" type="checkbox" name="fruits" value="grapes"><label for="fury-fruits-1-1">Grapes</label><br></fieldset>     
+What's your email?</label><input id="email-0" type="email" title="We need your email for verification." name="email" required><!--         This is another comment. --><label for="1">What fruits do you like?</label><fieldset id="1"><input id="fruits-1-0" type="checkbox" name="fruits" value="apples"><label for="fruits-1-0">Apples</label><br><div><!--      This is an embedded comment. --></div><input id="fruits-1-1" type="checkbox" name="fruits" value="grapes"><label for="fruits-1-1">Grapes</label><br></fieldset>     
 
 </form><textarea>
-  3 > 2</textarea>`
+  Did you know that 3 > 2?</textarea>`
 
 const stuff = prettify(html, { tab_size: 4 })
 console.log(stuff)
