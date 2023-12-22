ef5a82d2ca1066e41e00baec9f6c2d20daf6c48a
Jason Creviston <the@j4w8n.com>
2023-12-22 13:21:03 -0500

    chore: tweak tests

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /* Testing. */
+import { closify } from './src/closify'
 import { entify } from './src/entify'
 import { minify } from './src/minify'
 import { prettify } from './src/prettify'
-const html = `<form id="3"     >     <!-- 
+
+const uhtml = `<form id="3"     >     <!-- 
       
 This is a comment. --><!-- This is a second comment. --><label for="email-0">
 
@@ -23,7 +25,49 @@ This is another paragraph.
 
  </textarea><textarea class="  more  stuff  ">    </textarea>`
 
-const stuff = prettify(html)
-//const stuff = entify(html, true)
-//const stuff = minify(html)
-console.log(stuff)
+const ehtml = `<textarea  >
+
+Did   you know that 3 >   2?
+
+This is another paragraph.   
+
+
+</textarea><textarea class="  more  stuff  ">    </textarea>`
+
+const phtml = `<form id="3">
+<!-- This is a comment. -->
+<!-- This is a second comment. -->
+<label for="email-0">What's your email?</label>
+<input id="email-0" type="email" title="We need your email for verification." name="email" required />
+<!-- This is another comment. -->
+<label for="1">What fruits do you like?</label>
+<fieldset id="1">
+  <input id="fruits-1-0" type="checkbox" name="fruits" value="apples" />
+  <label for="fruits-1-0">Apples</label>
+  <br />
+  <div>
+    <!-- This is an embedded comment. -->
+  </div>
+  <input id="fruits-1-1" type="checkbox" name="fruits" value="grapes" />
+  <label for="fruits-1-1">Grapes</label>
+  <br />
+</fieldset>
+</form>
+<textarea>Did you know that 3 &gt; 2?&#13;&#13;This is another paragraph.</textarea>
+<textarea class="more stuff"></textarea>`
+
+const chtml = `<br><input><br><input>`
+
+const prettified_html = prettify(uhtml)
+const entified_html = entify(ehtml, true)
+const minified_html = minify(phtml)
+const closified_html = closify(chtml)
+
+console.log('All pretty:')
+console.log(prettified_html, '\n')
+console.log('With entities:')
+console.log(entified_html, '\n')
+console.log('All minified:')
+console.log(minified_html, '\n')
+console.log('With self-closing:')
+console.log(closified_html)
