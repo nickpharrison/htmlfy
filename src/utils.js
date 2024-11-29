049d10e7b283eb5b72fa3fb93f4f401d287692e2
Jason Creviston <the@j4w8n.com>
2024-11-29 09:34:47 -0500

    fix: ignore behavior

--- src/utils.js
+++ src/utils.js
@@ -80,6 +80,8 @@ export const ignoreElement = (html, ignore, mode = 'protect') => {
 const protectElement = (match, capture) => {
   return match.replace(capture, (match) => {
     return match
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
       .replace(/\n/g, '&#10;')
       .replace(/\r/g, '&#13;')
       .replace(/\s/g, '&nbsp;')
@@ -96,6 +98,8 @@ const protectElement = (match, capture) => {
 const unprotectElement = (match, capture) => {
   return match.replace(capture, (match) => {
     return match
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>')
       .replace(/&#10;/g, '\n')
       .replace(/&#13;/g, '\r')
       .replace(/&nbsp;/g, ' ')
